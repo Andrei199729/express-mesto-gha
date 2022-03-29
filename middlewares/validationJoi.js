c05508ae57421bb9582b8dbd276370f8e64176aa
Andrei Rahalevich <https://github.com/Andrei199729/Prech.git>
2022-03-29 13:44:00 +0300

    validationJoi

--- middlewares/validationJoi.js
+++ middlewares/validationJoi.js
@@ -1,10 +1,15 @@
 const { celebrate, Joi } = require('celebrate');
 
+const validateUrl = (value, helpers) => {
+  const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/;
+  if (!regex.test(value)) {
+    return helpers.error('Ссылка не валидна');
+  }
+  return value;
+};
+
 const registerValid = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(20),
-    avatar: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
@@ -19,14 +24,14 @@ const loginValid = celebrate({
 
 const userAvatarValid = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
+    avatar: Joi.string().required().custom(validateUrl),
   }),
 });
 
 const createCardValid = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
+    link: Joi.string().required().custom(validateUrl),
   }),
 });
 
